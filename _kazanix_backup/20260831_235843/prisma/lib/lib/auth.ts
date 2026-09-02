5e500abeac214b7bc40c90ffc356a50c7b80df1b
doganayc1 <GITHUB_EMAILIN>
2026-09-02 19:10:51 +0300

    Kazanix final production cleanup

--- _kazanix_backup/20260831_235843/prisma/lib/lib/auth.ts
+++
@@ -1,9 +0,0 @@
-import { AuthOptions } from "next-auth";
-
-export const authOptions: AuthOptions = {
-  providers: [],
-  session: {
-    strategy: "jwt",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
